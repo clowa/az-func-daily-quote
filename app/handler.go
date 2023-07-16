4d75f7f1fc5d615e54006ee72cfe230144f9d2f7
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2023-07-16 18:56:58 +0200

    add sample gin api

--- app/handler.go
+++ app/handler.go
@@ -1,27 +1,54 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gin-gonic/gin"
 )
 
-func helloHandler(w http.ResponseWriter, r *http.Request) {
-	message := "This HTTP triggered function executed successfully. Pass a name in the query string for a personalized response.\n"
-	name := r.URL.Query().Get("name")
-	if name != "" {
-		message = fmt.Sprintf("Hello, %s. This HTTP triggered function executed successfully.\n", name)
+func APIPort() string {
+	port := ":8080"
+	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
+		port = ":" + val
 	}
-	fmt.Fprint(w, message)
+	return port
+}
+
+func sayHello(c *gin.Context) {
+	log.Println("Invoke ROOT")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Hello, from Azure",
+	})
+	// return
+}
+
+func about(c *gin.Context) {
+	log.Println("Invoke ABOUT")
+	routes := [...]string{"/", "/about", "/health"}
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Exported routes",
+		"routes":  routes,
+	},
+	)
+	// return
+}
+
+func health(c *gin.Context) {
+	log.Println("Invoke HEALTH")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Everything is fantastic with this service",
+	})
 }
 
 func main() {
-	listenAddr := ":8080"
-	if val, ok := os.LookupEnv("FUNCTIONS_CUSTOMHANDLER_PORT"); ok {
-		listenAddr = ":" + val
-	}
-	http.HandleFunc("/api/HttpExample", helloHandler)
-	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+	router := gin.Default()
+	router.GET("api/simpleapi", sayHello)
+	router.GET("api/simpleapi/health", health)
+	router.GET("api/simpleapi/about", about)
+
+	port_info := APIPort()
+	router.Run(port_info)
+	log.Println("API is up & running - " + port_info)
 }
