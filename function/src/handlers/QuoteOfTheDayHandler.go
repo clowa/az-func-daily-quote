32a4f87b8bf92c1ca1ab31614b54e1be64a1e65b
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-03-18 19:18:22 +0100

    capsule cosmos connection logic into function

--- function/src/handlers/QuoteOfTheDayHandler.go
+++ function/src/handlers/QuoteOfTheDayHandler.go
@@ -38,28 +38,35 @@ func (q *Quote) Load(i *quotable.QuoteResponse) {
 	q.CreationDate = fmt.Sprintf("%d-%d-%d", now.Year(), int(now.Month()), now.Day())
 }
 
-func writeQuoteToDatabase(q *Quote) error {
+// Connects to the CosmosDB instance and returns a container client. Configuration in loaded from the environment.
+func connect() *azcosmos.ContainerClient {
 	config := config.GetConfig()
 
 	credential, err := azidentity.NewManagedIdentityCredential(nil)
 	if err != nil {
-		return err
+		log.Warnf("Error creating managed identity credential: %s", err)
 	}
 
 	client, err := azcosmos.NewClient(config.CosmosHost, credential, nil)
 	if err != nil {
-		return err
+		log.Warnf("Error creating Cosmos client: %s", err)
 	}
 
 	database, err := client.NewDatabase(config.CosmosDatabase)
 	if err != nil {
-		return err
+		log.Warnf("Error creating Cosmos database: %s", err)
 	}
 
 	container, err := database.NewContainer(config.CosmosContainer)
 	if err != nil {
-		return err
+		log.Warnf("Error creating Cosmos container: %s", err)
 	}
+
+	return container
+}
+
+func writeQuoteToDatabase(q *Quote) error {
+	container := connect()
 
 	partitionKey := azcosmos.NewPartitionKeyString(q.AuthorSlug)
 
@@ -70,6 +77,7 @@ func writeQuoteToDatabase(q *Quote) error {
 
 	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
 	defer cancel()
+
 	_, err = container.UpsertItem(ctx, partitionKey, bytes, nil) // ToDo: change to CreateItem()
 	if err != nil {
 		return err
@@ -77,6 +85,37 @@ func writeQuoteToDatabase(q *Quote) error {
 
 	return nil
 }
+
+// func getQuoteFromDatabase(creationDate string) (Quote, error) {
+// 	config := config.GetConfig()
+// 	container := connect()
+
+// 	ctx, cancel := context.WithTimeout(context.Background(), 4*time.Second)
+// 	defer cancel()
+
+// 	query := fmt.Sprintf("SELECT * FROM %s q WHERE q.creationDate = \"%s\" OFFSET 0 LIMIT 1", config.CosmosContainer, creationDate)
+// 	partitionKey := azcosmos.NewPartitionKeyString("albert-einstein")
+// 	opt := azcosmos.QueryOptions{
+// 		PageSizeHint: 1,
+// 	}
+// 	pager := container.NewQueryItemsPager(query, partitionKey, &opt)
+
+// 	var quotes []Quote
+// 	for pager.More() {
+// 		queryResponse, err := pager.NextPage(ctx)
+// 		if err != nil {
+// 			log.Warnf("Error querying database: %s", err)
+// 		}
+
+// 		for _, item := range queryResponse.Items {
+// 			json.Unmarshal(item, &quotes)
+// 		}
+// 	}
+// 	quote := quotes[0]
+// 	log.Printf("Query response: %v", quote)
+
+// 	return quote, nil
+// }
 
 func QuoteOfTheDayHandler(w http.ResponseWriter, r *http.Request) {
 	quotes, err := quotable.GetRandomQuote(quotable.GetRandomQuoteQueryParams{Limit: 1, Tags: []string{"technology"}})
