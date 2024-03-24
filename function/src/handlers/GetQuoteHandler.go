469f2732379da1aa35f3bcc226e6b6a88298ec4b
Cedric Ahlers <55838528+clowa@users.noreply.github.com>
2024-03-24 18:39:54 +0100

    move quote components to central router file

--- function/src/handlers/GetQuoteHandler.go
+++ function/src/handlers/GetQuoteHandler.go
@@ -20,29 +20,6 @@ const (
 	defaultContextTimeout = 10 * time.Second
 )
 
-// Struct representing the structure returned from the quotable API
-type Quote struct {
-	Id           string   `json:"id"`
-	Content      string   `json:"content"`
-	Author       string   `json:"author"`
-	AuthorSlug   string   `json:"authorSlug"`
-	Length       int      `json:"length"`
-	Tags         []string `json:"tags"`
-	CreationDate string   `json:"creationdate"`
-}
-
-func (q *Quote) Load(i *quotable.QuoteResponse) {
-	q.Id = i.Id
-	q.Content = i.Content
-	q.Author = i.Author
-	q.AuthorSlug = i.AuthorSlug
-	q.Length = i.Length
-	q.Tags = i.Tags
-
-	today := time.Now().Format("2006-01-02")
-	q.CreationDate = today
-}
-
 // Connects to the CosmosDB instance and returns a container client. Configuration in loaded from the environment.
 func connect() *mongo.Client {
 	config := config.GetConfig()
@@ -109,7 +86,7 @@ func getQuoteFromDatabase(creationDate string) (Quote, error) {
 	return quote, nil
 }
 
-func QuoteOfTheDayHandler(w http.ResponseWriter, r *http.Request) {
+func getQuoteHandler(w http.ResponseWriter, r *http.Request) {
 	var quoteOfTheDay Quote
 
 	today := time.Now().Format("2006-01-02")
--- function/src/main.go
+++ function/src/main.go
@@ -14,7 +14,8 @@ func main() {
 	globalConfig.PrintConfig()
 
 	listenAddr := globalConfig.ApiPort
-	http.HandleFunc("/api/quote", handlers.QuoteOfTheDayHandler)
+
+	http.HandleFunc("/api/quote", handlers.QuoteRouter)
 	log.Printf("About to listen on %s. Go to https://127.0.0.1%s/", listenAddr, listenAddr)
 	log.Fatal(http.ListenAndServe(listenAddr, nil))
 }
